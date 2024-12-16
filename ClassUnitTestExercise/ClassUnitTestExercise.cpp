7ffb39e70898f9cad30479fa88dea316233d075e
Luke Monesmith <LDMonesmith@student.fullsail.edu>
2024-12-16 14:19:49 -0500

    Addition func

--- ClassUnitTestExercise/ClassUnitTestExercise.cpp
+++ ClassUnitTestExercise/ClassUnitTestExercise.cpp
@@ -3,18 +3,17 @@
 
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+
+float Add(float left_operand, float right_operand) 
+{ 
+float result = left_operand + right_operand;
+return result; 
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+int main()
+{
+	std::cout << Add(3.5, 7.8);
+}
+
+
