24d1b94aad34fb73da7d4b4cedf559179ac5a5fa
Dylan-Ll-T <Dyllanostorres@student.fullsail.edu>
2024-12-16 14:40:59 -0500

    UnitTests

--- UnitTest1/UnitTest1.cpp
+++ UnitTest1/UnitTest1.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include "CppUnitTest.h"
+#include "../ClassUnitTestExercise/ClassUnitTestExercise.cpp"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -9,8 +10,19 @@ namespace UnitTest1
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(AddTestEqual)
 		{
+			
+			Assert::AreEqual((int)Add(1, 1), 2);
+	
+
+		}
+		TEST_METHOD(AddTestNotEqual)
+		{
+
+			Assert::AreNotEqual((int)Add(1, 1), 3);
+
+
 		}
 	};
 }
